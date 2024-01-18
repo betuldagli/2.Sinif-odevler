d00e9d19e91e5f65e9b60c56cd5bf7581c05fe3e
Betül Dağlı <125824370+betuldagli@users.noreply.github.com>
2024-01-18 20:37:19 +0300

    Add files via upload

---
+++ Kargo.java
@@ -0,0 +1,14 @@
+class Kargo {
+    String takipNo;
+    String gonderici;
+    String alici;
+    String kargoDurum;
+
+    public Kargo(String takipNo, String gonderici, String alici,String kargoDurum) {
+        this.takipNo = takipNo;
+        this.gonderici = gonderici;
+        this.alici = alici;
+        this.kargoDurum = kargoDurum;
+
+    }
+}
